a9f0f522bffc15037e0d5772cc30b49d3985b0b8
Tom Walls <tomwalls33@gmail.com>
2018-02-12 08:32:49 +0000

    angular cli proxy config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,5 +6,5 @@
 export const environment = {
   production: false,
   //api_url: 'https://conduit.productionready.io/api'
-  api_url: 'http://localhost:62378'
+  api_url: 'http://localhost:4200/api'
 };
